5bbd57a958fffff5f52a951044da837eb8cc2f1e
Arushi Ballav <71352126+arushiballav24036@users.noreply.github.com>
2024-08-12 10:37:51 +0530

    Add files via upload

---
+++ query2.sql
@@ -0,0 +1,25 @@
+ INSERT INTO Winners (category, winner_name, movie_title, year)
+    VALUES 
+    ('Best Picture', 'OPPENHEIMER', 'OPPENHEIMER', 2024),
+    ('Actor in a Leading Role', 'Cillian Murphy', 'OPPENHEIMER', 2024),
+    ('Actor in a Supporting Role', 'Robert Downey Jr.', 'OPPENHEIMER', 2024),
+    ('Actress in a Leading Role', 'Emma Stone', 'POOR THINGS', 2024),
+    ('Actress in a Supporting Role', 'Da\Vine Joy Randolph', 'THE HOLDOVERS', 2024),
+    ('Animated Feature Film', 'THE BOY AND THE HERON', 'THE BOY AND THE HERON', 2024),
+    ('Cinematography', 'Hoyte van Hoytema', 'OPPENHEIMER', 2024),
+    ('Costume Design', 'Holly Waddington', 'POOR THINGS', 2024),
+    ('Directing', 'Christopher Nolan', 'OPPENHEIMER', 2024),
+    ('Documentary Short Film', 'THE LAST REPAIR SHOP', 'THE LAST REPAIR SHOP', 2024),
+    ('Film Editing', 'Jennifer Lame', 'OPPENHEIMER', 2024),
+    ('International Feature Film', 'THE ZONE OF INTEREST', 'THE ZONE OF INTEREST', 2024),
+    ('Makeup and Hairstyling', 'Nadia Stacey, Mark Coulier and Josh Weston', 'POOR THINGS', 2024),
+    ('Music (Original Score)', 'Ludwig Göransson', 'OPPENHEIMER', 2024),
+    ('Music (Original Song)', 'What Was I Made For?', 'BARBIE', 2024),
+    ('Production Design', 'James Price and Shona Heath', 'POOR THINGS', 2024),
+    ('Short Film (Live Action)', 'Wes Anderson and Steven Rales', 'THE WONDERFUL STORY OF HENRY SUGAR', 2024),
+    ('Sound', 'Tarn Willers and Johnnie Burn', 'THE ZONE OF INTEREST', 2024),
+    ('Visual Effects', 'Takashi Yamazaki, Kiyoko Shibuya, Masaki Takahashi and Tatsuji Nojima', 'GODZILLA MINUS ONE', 2024),
+    ('Writing (Adapted Screenplay)', 'Cord Jefferson', 'AMERICAN FICTION', 2024),
+    ('Writing (Original Screenplay)', 'Justine Triet and Arthur Harari', 'ANATOMY OF A FALL', 2024);
+    
+    select * from winners;
---
+++ query3.sql
@@ -0,0 +1,12 @@
+INSERT INTO Movies (title, producer, release_year) VALUES
+    ('AMERICAN FICTION', 'Ben LeClair, Nikos Karamigios, Cord Jefferson and Jermaine Johnson', 2023),
+    ('ANATOMY OF A FALL', 'Marie-Ange Luciani and David Thion', 2023),
+    ('BARBIE', 'David Heyman, Margot Robbie, Tom Ackerley and Robbie Brenner', 2023),
+    ('THE HOLDOVERS', 'Mark Johnson', 2023),
+    ('KILLERS OF THE FLOWER MOON', 'Dan Friedkin, Bradley Thomas, Martin Scorsese and Daniel Lupi', 2023),
+    ('MAESTRO', 'Bradley Cooper, Steven Spielberg, Fred Berner, Amy Durning and Kristie Macosko Krieger', 2023),
+    ('OPPENHEIMER', 'Emma Thomas, Charles Roven and Christopher Nolan', 2023),
+    ('PAST LIVES', 'David Hinojosa, Christine Vachon and Pamela Koffler', 2023),
+    ('POOR THINGS', 'Ed Guiney, Andrew Lowe, Yorgos Lanthimos and Emma Stone', 2023),
+    ('THE ZONE OF INTEREST', 'James Wilson', 2023);
+    select * from movies;
